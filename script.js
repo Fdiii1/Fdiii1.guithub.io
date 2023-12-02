885569b78940a6b725ec13a83f1ea7f88b6b74dc
Fdiii1 <phat76997@gmail.com>
2023-12-02 10:19:38 +0700

    hhhh

---
+++ script.js
@@ -0,0 +1,20 @@
+// const wrapper = document.querySelector('.wrapper');
+// const loginLink = document.querySelector('.login-link');
+// const registerLink = document.querySelector('.register-link');
+
+// registerLink.addEventListener('click', ()=> {
+//     wrapper.classList.add('.active');
+
+// })  ;
+const register = () => {
+    wrapper.classList.add('.active');
+}
+const login = () => {
+    wrapper.classList.remove('.active');
+}
+// loginLink.addEventListener('click',()=>{
+//     wrapper.classList.remove('.active');
+// })  ;
+const btnLogin = () => {
+    wrapper.classList.add('.active');
+}
